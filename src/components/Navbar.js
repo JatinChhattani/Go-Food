a7bffb017d8b09ec7d15d942acb4f574ab30b8d4
Jatin chhattani <jatinchattani25@gmail.com>
2023-08-09 21:37:26 +0530

    Add to cart Functionality feature implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link,useNavigate } from 'react-router-dom'
+import Badge from 'react-bootstrap/Badge'
+import Cart from '../screens/Cart';
+import Modal from '../Modal';
+import { useCart } from './ContextReducer';
+
 export default function Navbar() {
+  const [cartView,setCartView] = useState(false)
+  let data = useCart();
   const navigate = useNavigate();
   const handleLogout = ()=>{
     localStorage.removeItem("authToken");
@@ -34,9 +41,11 @@ export default function Navbar() {
               </div>
               :
               <div>
-                <div className="btn bg-white text-success mx-2">
-                  Cart
+                <div className="btn bg-white text-success mx-2" onClick={()=>{setCartView(true)}}> 
+                  My Cart  {""}
+                  <Badge pill bg="danger">{data.length}</Badge>
                 </div>
+                {cartView? <Modal onClose={()=>setCartView(false)} > <Cart/> </Modal> :null}
                 <div className="btn bg-white text-danger mx-2" onClick={handleLogout}>
                   Logout
                 </div>
